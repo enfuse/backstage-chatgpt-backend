7b9bcd182ed86a9fe1ad9b45f12de75957aac5a1
ch-enfuse <chr@enfuse.io>
2023-04-13 14:42:11 -0700

    Change to zero shot prompting.

--- src/service/openai.ts
+++ src/service/openai.ts
@@ -18,38 +18,6 @@ export const openAPIResponse =  async (input : ChatGPTUserInput) => {
           content: 'You are a smart code generation assistant that understands user requirements and generates files in various programming languages, frameworks, and file formats accordingly.'
         },
         {
-          role: 'user',
-          content: 'A java spring controler to serve as a payments endpoint for a pet store'
-        },
-        {
-          role: 'assistant',
-          content: `import org.springframework.web.bind.annotation.*;
-          import java.util.HashMap;
-          import java.util.Map;
-          
-          @RestController
-          @RequestMapping("/payments")
-          public class PaymentsController {
-          
-              @PostMapping("/charge")
-              public Map<String, Object> chargeCard(@RequestBody Map<String, Object> paymentMap) {
-                  Double amount = Double.parseDouble(paymentMap.get("amount").toString());
-                  String currency = paymentMap.get("currency").toString();
-                  String token = paymentMap.get("token").toString();
-          
-                  // Charge the card using the payment gateway API
-                  // ...
-          
-                  Map<String, Object> charge = new HashMap<>();
-                  charge.put("amount", amount);
-                  charge.put("currency", currency);
-                  charge.put("status", "approved");
-          
-                  return charge;
-              }
-          }`
-        },
-        {
           role:'user',
           content : `${input.description}`
         } ]
