f1347d4d1857b34287d832b2b61838355881d4d1
Chrsitian Herrera <chr@enfuse.io>
2023-08-02 18:01:05 -0700

    Changes for local development

--- src/service/standaloneServer.ts
+++ src/service/standaloneServer.ts
@@ -14,7 +14,7 @@ export interface ServerOptions {
 export async function startStandaloneServer(
   options: ServerOptions,
 ): Promise<Server> {
-  const logger = options.logger.child({ service: 'chatgpt-backend-backend' });
+  const logger = options.logger.child({ service: 'chatgpt-backend' });
   const config = await loadBackendConfig({ logger, argv: process.argv });
 
   const router = await createRouter({
@@ -24,10 +24,9 @@ export async function startStandaloneServer(
 
   let service = createServiceBuilder(module)
     .setPort(options.port)
-    .addRouter('/chatgpt-backend', router);
-  if (options.enableCors) {
+    .addRouter('/api/chatgpt', router);
     service = service.enableCors({ origin: 'http://localhost:3000' });
-  }
+
 
   return await service.start().catch(err => {
     logger.error(err);
